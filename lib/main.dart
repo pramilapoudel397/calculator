69afd12ae7aa8085c1e67b42037b1c6846932489
Pramila Poudel <pramila@supremeitsolutions.com>
2021-07-02 13:56:54 +0545

    Simple Calculator App

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:calculator/views/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Calculator());
 }
 
-class MyApp extends StatelessWidget {
+class Calculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
